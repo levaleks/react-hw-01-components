35ce6862d0228fa3f3fdab027083e58f3a78df54
Oleksii Levchenko <>
2020-09-06 00:19:08 +0300

    Create app with CRA and customize it

---
+++ src/components/App/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const WelcomeText = styled.p`
+    color: deepskyblue;
+`;
+
+export const App: React.FC = () => {
+    return <WelcomeText>
+
+
+
+
+
+
+
+
+        Hello</WelcomeText>;
+};
