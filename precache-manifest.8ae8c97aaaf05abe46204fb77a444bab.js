3b95346fbd71ad7b231f751c8ef6b0a63e5965c1
Oleksii Levchenko <>
2020-09-08 08:55:00 +0300

    Updates

---
+++ precache-manifest.8ae8c97aaaf05abe46204fb77a444bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f99aaa02e15ed3a724b7dc29703119",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "82c555afe2916d9a1821",
+    "url": "/react-hw-01-components/static/css/main.5fa49014.chunk.css"
+  },
+  {
+    "revision": "28eb41e4ea14049880a1",
+    "url": "/react-hw-01-components/static/js/2.a807e140.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/react-hw-01-components/static/js/2.a807e140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c555afe2916d9a1821",
+    "url": "/react-hw-01-components/static/js/main.277a3eab.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
